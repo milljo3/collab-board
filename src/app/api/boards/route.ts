ec875ebe5302007acc49985cf4d60f1373f7c1b6
joey102522 <joeykm91@gmail.com>
2025-07-22 19:04:14 -0400

    feat: boards GET + POST routes

---
+++ src/app/api/boards/route.ts
@@ -0,0 +1,40 @@
+import {NextRequest, NextResponse} from "next/server";
+import {auth} from "@/lib/auth";
+import {RedisAllBoardService} from "@/lib/redis-board-service";
+
+export async function GET(req: NextRequest) {
+    try {
+        const session = await auth.api.getSession({ headers: req.headers });
+
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized", status: 401 });
+        }
+
+        const boards = await RedisAllBoardService.getAllBoards(session.user.id);
+        if (!boards) return NextResponse.json({ error: "Error getting boards", status: 404 });
+
+        return NextResponse.json(boards);
+    }
+    catch(err) {
+        console.log(err);
+        return NextResponse.json({ error: "Internal Server Error", status: 500 });
+    }
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const session = await auth.api.getSession({ headers: req.headers });
+
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized", status: 401 });
+        }
+
+        await RedisAllBoardService.createBoard(session.user.id, "");
+
+        return NextResponse.json("Board created successfully.");
+    }
+    catch(err) {
+        console.log(err);
+        return NextResponse.json({ error: "Internal Server Error", status: 500 });
+    }
+}
