50ea20a7b6ada9415df37b572e2eb1e182727237
joey102522 <joeykm91@gmail.com>
2025-07-26 20:29:22 -0400

    fix: fixed query key

--- src/hooks/useBoardQuery.ts
+++ src/hooks/useBoardQuery.ts
@@ -4,7 +4,7 @@ import {fetchBoard} from "@/api/board/fetch-board";
 
 export const useBoardQuery = (id?: string) => {
     return useQuery<Board>({
-        queryKey: ["meeting", id],
+        queryKey: ["board", id],
         queryFn: () => fetchBoard(id!),
         enabled: !!id
     });
