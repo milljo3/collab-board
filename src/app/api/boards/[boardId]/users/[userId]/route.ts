a2f3fd461d9417c9ecb5697799915831aaeb95da
joey102522 <joeykm91@gmail.com>
2025-07-29 19:11:01 -0400

    feat: GET, POST, PATCH, DELETE user board routes

---
+++ src/app/api/boards/[boardId]/users/[userId]/route.ts
@@ -0,0 +1,101 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { auth } from "@/lib/auth"; // or wherever your auth comes from
+import { updateBoardUserSchema } from "@/types/board";
+import { RedisUserService } from "@/lib/redis-user-service";
+import {RedisBoardUserService} from "@/lib/redis-board-user-service";
+
+export async function PATCH(req: NextRequest) {
+    try {
+        const session = await auth.api.getSession({ headers: req.headers });
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const boardId = getBoardId(req);
+        const targetUserId = getUserId(req);
+
+        if (session.user.id === targetUserId) {
+            return NextResponse.json({ error: "You cannot modify your own role."}, { status: 403 });
+        }
+
+        const access = await RedisUserService.requireBoardAccess(boardId, session.user.id, "MANAGE_USERS");
+        if (!access) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const body = await req.json();
+        const parsed = updateBoardUserSchema.parse(body);
+
+        if (parsed.role === Role.OWNER) {
+            return NextResponse.json({ error: "Can't add another owner"}, { status: 401 });
+        }
+
+        const updated = await prisma.boardUser.update({
+            where: {
+                userId_boardId: {
+                    userId: targetUserId,
+                    boardId: boardId,
+                },
+            },
+            data: {
+                role: parsed.role,
+            },
+        });
+
+        await RedisBoardUserService.refreshBoardUsersCache(boardId);
+
+        return NextResponse.json({ success: true, boardUser: updated });
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ error: "Internal Server Error"}, { status: 500 });
+    }
+}
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const session = await auth.api.getSession({ headers: req.headers });
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const boardId = getBoardId(req);
+        const targetUserId = getUserId(req);
+
+        if (session.user.id === targetUserId) {
+            return NextResponse.json({ error: "You cannot remove yourself."}, { status: 403 });
+        }
+
+        const access = await RedisUserService.requireBoardAccess(boardId, session.user.id, "MANAGE_USERS");
+        if (!access) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        await prisma.boardUser.delete({
+            where: {
+                userId_boardId: {
+                    userId: targetUserId,
+                    boardId: boardId,
+                },
+            },
+        });
+
+        await RedisBoardUserService.refreshBoardUsersCache(boardId);
+
+        return NextResponse.json({ success: true });
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ error: "Internal Server Error"}, { status: 500 });
+    }
+}
+
+
+function getBoardId(req: NextRequest) {
+    const url = new URL(req.url);
+    return url.pathname.split("/")[3];
+}
+
+function getUserId(req: NextRequest) {
+    const url = new URL(req.url);
+    return url.pathname.split("/").pop()!;
+}
---
+++ src/app/api/boards/[boardId]/users/route.ts
@@ -0,0 +1,99 @@
+import {NextRequest, NextResponse} from "next/server";
+import {auth} from "@/lib/auth";
+import {RedisUserService} from "@/lib/redis-user-service";
+import {RedisBoardUserService} from "@/lib/redis-board-user-service";
+import {addBoardUserSchema} from "@/types/board";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req: NextRequest) {
+    try {
+        const session = await auth.api.getSession({ headers: req.headers });
+
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const boardId = getBoardId(req);
+
+        const access = await RedisUserService.requireBoardAccess(boardId, session.user.id, "VIEW_BOARD");
+        if (!access) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const boardUsers = await RedisBoardUserService.getBoardUsers(boardId);
+        if (!boardUsers) return NextResponse.json({ error: "Error getting board users"}, { status: 404 });
+
+        return NextResponse.json(boardUsers);
+    }
+    catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: "Internal Server Error"}, { status: 500 });
+    }
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const session = await auth.api.getSession({ headers: req.headers });
+
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const boardId = getBoardId(req);
+
+        const access = await RedisUserService.requireBoardAccess(boardId, session.user.id, "MANAGE_USERS");
+        if (!access) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const body = await req.json();
+        const parsed = addBoardUserSchema.parse(body);
+
+        const user = await prisma.user.findUnique({
+            where: {email: parsed.email},
+        });
+
+        if (!user) return NextResponse.json({ error: "User not found."}, { status: 404 });
+
+        const existing = await prisma.boardUser.findUnique({
+            where: {
+                userId_boardId: {
+                    userId: user.id,
+                    boardId: boardId,
+                },
+            },
+        });
+
+        if (existing) {
+            return NextResponse.json({ error: "User already in board."}, { status: 400 });
+        }
+
+        const boardUser = await prisma.boardUser.create({
+            data: {
+                boardId,
+                userId: user.id,
+                role: parsed.role,
+            },
+        });
+
+        await RedisBoardUserService.refreshBoardUsersCache(boardId);
+
+        return NextResponse.json(boardUser);
+    }
+    catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: "Internal Server Error"}, { status: 500 });
+    }
+}
+
+function getBoardId(req: NextRequest) {
+    const url = new URL(req.url);
+    const split = url.pathname.split("/");
+
+    const id = split[3];
+    if (!id) {
+        throw new Error("Board not found");
+    }
+
+    return id;
+}
